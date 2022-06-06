a273101db2862d3310f2ca2bc8a4a0889903face
Kai <106977950+k-falcone@users.noreply.github.com>
2022-06-06 10:39:27 -0400

    Update streamlit_app.py
    added fruit example

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,5 +17,5 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Pick List for Fruit Smoothie
-sl.multiselect('Pick some fruits:', list(my_fruit_list.index))
+sl.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 sl.dataframe(my_fruit_list)

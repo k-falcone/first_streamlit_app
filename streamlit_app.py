2d85bcfa2722608321000a954554bd2b29615616
Kai <106977950+k-falcone@users.noreply.github.com>
2022-06-07 10:49:44 -0400

    Update streamlit_app.py
    added snowflake data

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,10 @@ fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # output to the screen as a table
 sl.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**sl.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+sl.text("Hello from Snowflake:")
+sl.text(my_data_row)
